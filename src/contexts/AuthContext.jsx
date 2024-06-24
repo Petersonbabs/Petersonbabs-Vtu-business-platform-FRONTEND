9105fe9476019e5878bafeffc8fbc84cc5a26bf2
petersonbabs <timiebabs@gmail.com>
2024-06-24 22:57:02 +0100

    Refactoring code

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -10,13 +10,13 @@ import cryptoJs from "crypto-js";
 //     const response = await axios();
 //     const data = response.data
 //     if(data.status == 'success'){
-      
+
 //     }
-    
+
 //   } catch (error) {
 //     console.log(error.response.data.message)
 //     alert(error.response.data.message)
-    
+
 //   }finally{
 //     console.log('done')
 //     setIsLoading(false)
@@ -35,7 +35,6 @@ const AuthProvider = ({ children }) => {
   const [isLoading, setIsLoading] = useState(false);
   const navigate = useNavigate();
 
-
   const setUserData = (data) => {
     localStorage.setItem("token", data.token);
     localStorage.setItem("user", JSON.stringify(data.user));
@@ -44,24 +43,27 @@ const AuthProvider = ({ children }) => {
   };
 
   const apiUrl = import.meta.env.VITE_apiUrl;
-
+  
   // REGISTER USER
   const signUp = async (formData) => {
     setIsLoading(true);
 
     try {
       const response = await axios.post(`${apiUrl}/auth/signup`, formData);
+      console.log(response);
       const data = response.data;
-      console.log(data.status);
+
       if (data.status == "success") {
+
         setUserData(data);
+        console.log(import.meta.env.VITE_EMAIL_SECRET);
         const encryptedEmail = cryptoJs.AES.encrypt(
           formData.email,
           import.meta.env.VITE_EMAIL_SECRET
         ).toString();
 
         navigate(`/verify-email/${encodeURIComponent(encryptedEmail)}`);
-        window.location.reload()
+        window.location.reload();
         alert(data.message);
       }
     } catch (error) {
@@ -76,7 +78,7 @@ const AuthProvider = ({ children }) => {
   // SIGN IN
   const signIn = async (formData) => {
     console.log("login in...");
-    setIsLoading(true)
+    setIsLoading(true);
     try {
       const response = await axios.post(`${apiUrl}/auth/login`, formData);
       console.log(response);
@@ -85,29 +87,33 @@ const AuthProvider = ({ children }) => {
       if (data.status == "success") {
         setUserData(data);
         navigate("/");
-        window.location.reload()
+        window.location.reload();
         alert(data.message);
       }
     } catch (error) {
       console.log(error.response.data.message);
       alert(error.response.data.message);
     } finally {
-      setIsLoading(false)
+      setIsLoading(false);
       console.log("done!");
     }
   };
 
   // LOGOUT
   const logout = async () => {
-    console.log('loging out...')
+    console.log("loging out...");
     try {
-      const response = await axios.post(`${apiUrl}/auth/logout`, {token}, {headers: {Authorization: `Bearer ${token}`}})
+      const response = await axios.post(
+        `${apiUrl}/auth/logout`,
+        { token },
+        { headers: { Authorization: `Bearer ${token}` } }
+      );
       const data = await response.data;
       if (data.status == "success") {
-        localStorage.clear('user');
-        localStorage.clear('token');
+        localStorage.clear("user");
+        localStorage.clear("token");
         navigate("/");
-        window.location.reload()
+        window.location.reload();
         alert(data.message);
       }
     } catch (error) {
@@ -120,43 +126,44 @@ const AuthProvider = ({ children }) => {
 
   // FORGOT PASSWORD
   const forgotPassword = async (formData) => {
-    setIsLoading(true)
+    setIsLoading(true);
     try {
-      const response = await axios.post(`${apiUrl}/auth/forgot-password`, formData);
-      const data = response.data
-      if(data.status == 'success'){
-
+      const response = await axios.post(
+        `${apiUrl}/auth/forgot-password`,
+        formData
+      );
+      const data = response.data;
+      if (data.status == "success") {
       }
-      
     } catch (error) {
-      console.log(error)
-      alert(error.response.data.message)
-      
-    }finally{
-      console.log('done')
-      setIsLoading(false)
+      console.log(error);
+      alert(error.response.data.message);
+    } finally {
+      console.log("done");
+      setIsLoading(false);
     }
-  }
+  };
 
   // RESET PASSWORD
   const resetPassword = async (email) => {
-    setIsLoading(true)
+    setIsLoading(true);
     try {
-      const response = await axios.post(`${apiUrl}/auth/change-password`, email);
-      const data = await response.data
+      const response = await axios.post(
+        `${apiUrl}/auth/change-password`,
+        email
+      );
+      const data = await response.data;
       if (data.status == "success") {
-       
         alert(data.message);
       }
-
     } catch (error) {
-      console.log(error.response.data.message)
-      alert(error.response.data.message)
+      console.log(error.response.data.message);
+      alert(error.response.data.message);
     } finally {
-      setIsLoading(false)
-      console.log('done!');
+      setIsLoading(false);
+      console.log("done!");
     }
-  }
+  };
 
   const value = {
     user,
